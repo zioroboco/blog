795681600da87f60710fb335e8912f4d12966d17
zioroboco <zioroboco@gmail.com>
2018-04-14 20:35:48 +1000

    Add 404 page stub

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+export default () => (
+  <div style={{
+    textAlign: "center",
+    fontSize: "10rem",
+    transform: "translateY(50%)"
+  }}>
+    😺
+  </div>
+)
