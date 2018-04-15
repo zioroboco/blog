171718f5e6d637a05d9507947a8db117bef7a390
zioroboco <zioroboco@gmail.com>
2018-04-16 00:24:10 +1000

    Add base typographic styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = {
     title: "ziorobo.co"
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/styles/typography.js"
+      }
+    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript"
   ]
---
+++ src/styles/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography"
+import irving from "typography-theme-irving"
+
+const typography = new Typography(irving)
+
+export default typography
