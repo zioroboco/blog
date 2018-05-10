77c26c12606a80cbcfda1fe8375f69a923c2d5ec
zioroboco <zioroboco@gmail.com>
2018-05-10 21:28:55 +1000

    Add header and basic single-column styles

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,8 +2,16 @@ import * as React from "react"
 import Helmet from "react-helmet"
 
 export default ({ children }) => (
-  <div>
-    <Helmet title="ziorobo.co" />
+  <div className="content">
+    <Helmet
+      title="ziorobo.co"
+      style={[
+        "https://fonts.googleapis.com/css?family=Montserrat+Alternates",
+        require("../styles/content.css"),
+        require("../styles/base.css")
+      ]}
+    />
+    <div id="header">ziorobo.co</div>
     {children()}
   </div>
 )
